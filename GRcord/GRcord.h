08bb30e062367de9aa0780e13ebd791b2b9ace02
Zhigang Gao <gzg1984@aliyun.com>
2017-12-30 12:24:48 +0800

    Initial Commit

---
+++ GRcord/GRcord.h
@@ -0,0 +1,13 @@
+//
+//  GRcord.h
+//  GRcord
+//
+//  Created by 高志刚 on 2017/12/30.
+//  Copyright © 2017年 高志刚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GRcord : NSObject
+
+@end
